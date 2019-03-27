3fbe877878dfd132adbdaad107b87e7791e1f4f9
Mallika Mitra <mallika.mitra@journalism.cuny.edu>
2019-03-26 20:48:58 -0400

    added retailers

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $("body").on("change", ".filter", function (e) {
 	e.preventDefault();
+
 	console.log("Hello, world!");
+
 	const $this_filter = $(this);
 	const year_selected = $this_filter.val();
+
 	console.log("Year Selected " + year_selected)
-	$(".item.active").removeClass("active");
+
+	$(".year.active").removeClass("active");
 	$("." + year_selected).addClass("active");
 
-})
+});
